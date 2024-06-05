5ab2dfd458d83f79582048f3fba34faeeef84d55
josedonassolo <103005497+josedonassolo@users.noreply.github.com>
2024-06-05 15:51:59 -0300

    Update GamePanel.java

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -101,7 +101,7 @@ public class GamePanel extends JPanel implements ActionListener {
 
 		updateValues();
 	}
-
+	@Override
 	public void actionPerformed(ActionEvent evt) {
 		String act = evt.getActionCommand();
 
@@ -113,6 +113,7 @@ public class GamePanel extends JPanel implements ActionListener {
 			playDouble();
 		} else if (act.equals("Stand")) {
 			stand();
+			resetGamePanel();
 		} else if (isBetEvent(act)) {
 			increaseBet(Integer.parseInt(act));
 		} else if (act.equals("Clear")) {
@@ -149,6 +150,13 @@ public class GamePanel extends JPanel implements ActionListener {
 
 	public void clearBet() {
 		player.clearBet();
+	}
+
+	public void resetGamePanel() {
+		currentBet.setText("");
+		playerWallet.setText("");
+		cardsLeft.setText("");
+		dealerSays.setText("");
 	}
 
 	public void updateValues() {
